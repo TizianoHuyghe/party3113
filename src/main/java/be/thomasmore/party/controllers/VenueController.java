0f95fb9b905ec231c2e6abc88f6700b5bdc2ffce
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-01-31 17:55:51 +0100

    chapter 2: venuedetails with PathVariable

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -14,9 +15,9 @@ public class VenueController {
     @Autowired
     private VenueRepository venueRepository;
 
-    @GetMapping("/venuedetails")
-    public String venuedetails(Model model) {
-        Optional<Venue> venueFromDb = venueRepository.findById(1);
+    @GetMapping("/venuedetails/{id}")
+    public String venuedetails(Model model, @PathVariable int id) {
+        Optional<Venue> venueFromDb = venueRepository.findById(id);
         if (venueFromDb.isPresent()) {
             model.addAttribute("venue", venueFromDb.get());
         }
