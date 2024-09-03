7f39470ce1b0587687474b14583400da1d97199b
Vera Peeters <vera.peeters@thomasmore.be>
2024-09-03 22:00:03 +0200

    chapter 1: show details for 1 venue (POJO) - venueName

---
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -0,0 +1,18 @@
+package be.thomasmore.party.controllers;
+
+import be.thomasmore.party.model.Venue;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VenueController {
+    @GetMapping("/venuedetails")
+    public String venuedetails(Model model) {
+        Venue venue = new Venue();
+        venue.setVenueName("BoesjKammeree");
+        venue.setLinkMoreInfo("https://www.facebook.com/boesjkammeree/");
+        model.addAttribute("venue", venue);
+        return "venuedetails";
+    }
+}
