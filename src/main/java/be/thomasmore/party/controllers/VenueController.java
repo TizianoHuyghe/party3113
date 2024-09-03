8e4743ed89c1fd883cb2853140c2f8d7b9c6c448
Vera Peeters <vera.peeters@thomasmore.be>
2024-09-03 22:10:50 +0200

    chapter 1: get venue from database
    (cherry picked from commit 7ad7312e34a1f4cbe2787a8309477879059746ec)

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -1,18 +1,25 @@
 package be.thomasmore.party.controllers;
 
 import be.thomasmore.party.model.Venue;
+import be.thomasmore.party.repositories.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class VenueController {
+    @Autowired
+    private VenueRepository venueRepository;
+
     @GetMapping("/venuedetails")
     public String venuedetails(Model model) {
-        Venue venue = new Venue();
-        venue.setVenueName("BoesjKammeree");
-        venue.setLinkMoreInfo("https://www.facebook.com/boesjkammeree/");
-        model.addAttribute("venue", venue);
+        Optional<Venue> venueFromDb = venueRepository.findById(1);
+        if (venueFromDb.isPresent()) {
+            model.addAttribute("venue", venueFromDb.get());
+        }
         return "venuedetails";
     }
 }
