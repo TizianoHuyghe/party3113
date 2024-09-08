497974d3a7ccfe2841a306ae2e2457feddf2d150
Vera Peeters <vera.peeters@thomasmore.be>
2024-09-08 16:33:26 +0200

    chapter 3: filter artists: simple keyword
    (cherry picked from commit 99837ec1e02c61b7ceed6294e0769320a00cabed)

--- src/main/java/be/thomasmore/party/controllers/ArtistController.java
+++ src/main/java/be/thomasmore/party/controllers/ArtistController.java
@@ -2,22 +2,30 @@ package be.thomasmore.party.controllers;
 
 import be.thomasmore.party.model.Artist;
 import be.thomasmore.party.repositories.ArtistRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class ArtistController {
+    final private Logger logger = LoggerFactory.getLogger(ArtistController.class);
+
     @Autowired
     private ArtistRepository artistRepository;
 
     @GetMapping("/artistlist")
-    public String artistList(Model model) {
-        Iterable<Artist> artists = artistRepository.findAll();
+    public String artistList(Model model,
+                             @RequestParam(required = false) String keyword) {
+        logger.info(String.format("artistList -- keyword=%s", keyword));
+        List<Artist> artists = artistRepository.findByKeyword(keyword);
         model.addAttribute("artists", artists);
         return "artistlist";
     }
--- src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
+++ src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
@@ -1,7 +1,16 @@
 package be.thomasmore.party.repositories;
 
 import be.thomasmore.party.model.Artist;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ArtistRepository extends CrudRepository<Artist, Integer> {
+    @Query("select a from Artist a WHERE " +
+            ":keyword IS NULL OR " +
+            "(a.artistName ILIKE CONCAT('%', :keyword, '%'))")
+    List<Artist> findByKeyword(@Param("keyword")String keyword);
+
 }
