612b7407675632307d80fcb714f5d27c2a73a470
Vera Peeters <vera.peeters@thomasmore.be>
2024-09-09 14:58:33 +0200

    chapter 5: form with method POST
    (cherry picked from commit 110669e754719bd8c3fe1f2044462d6b04f2bba9)

--- src/main/java/be/thomasmore/party/controllers/admin/PartyAdminController.java
+++ src/main/java/be/thomasmore/party/controllers/admin/PartyAdminController.java
@@ -7,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -39,4 +36,10 @@ public class PartyAdminController {
         return "admin/partyedit";
     }
 
+    @PostMapping("/partyedit/{id}")
+    public String partyEditPost(@PathVariable int id,
+                                Party party) {
+        logger.info("partyEditPost " + id + " -- new name=" + party.getName());
+        return "admin/partyedit";
+    }
 }
