4141992dfd1689e76d9a2442b472bf18ddc5f0b5
Vera Peeters <vera.peeters@thomasmore.be>
2024-09-13 18:47:04 +0200

    chapter 7: login: simple login/logout buttons on home page
    (cherry picked from commit 6bcec6d5c1adb60ea53f927927795b3263c6eb70)
    (cherry picked from commit 05d303bdda6f2d4d74950a2b2fa9664cc8afc62f)

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package be.thomasmore.party.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -12,8 +15,13 @@ import static java.time.LocalDateTime.now;
 
 @Controller
 public class HomeController {
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping({"/", "/home"})
-    public String home(Model model) {
+    public String home(Model model, Principal principal) {
+        final String loginName = principal != null ? principal.getName() : null;
+        logger.info("homepage - logged in as " + loginName);
+        model.addAttribute("loginName", loginName);
         return "home";
     }
 
